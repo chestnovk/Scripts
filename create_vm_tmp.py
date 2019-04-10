db3eb4a10765340d1ebdd4ee7b9683de8c8ea45e
kchestnov <chestnov.konstantin@yandex.ru>
2019-04-10 19:38:50 +0300

    Fix commandline add define(), start()

--- create_vm_tmp.py
+++ create_vm_tmp.py
@@ -7,12 +7,11 @@ import subprocess
 
 class VzVmBaseConfig:
 
-    def __init__(self, cluster_name):
+    def __init__(self, cluster_name, vm_iso_path):
 
         # Basic settings
-        #self.vm_iso_path = "/home/kchestnov/ISO/CentOS-7-x86_64-Minimal-1810.iso"
-        self.vm_iso_path = "/home/kchestnov/create_vm/CentOS-7-x86_64-NetInstall-1810.iso"
-        self.kickstart = "https://172.16.56.80/ks/example.cfg"
+        self.vm_iso_path = vm_iso_path
+        self.kickstart = "http://172.16.56.80/ks/example.cfg"
 
         self.public_mask = "255.255.252.0"
         self.public_gw = "172.16.56.1"
@@ -55,14 +54,13 @@ class VzVmConfig:
         self.ks = VzVmBaseConfig.kickstart
 
         # Management containers
-        self.includes_va = includes_va
-        self.va_register = va_register
-        self.includes_storage_ui = includes_storage_ui
-        self.storage_ui_register = storage_ui_register
-        self.storage_ui_token = storage_ui_token
-        self.va_ip = va_ip
-        self.storage_ui_ip = storage_ui_ip
-        self.storage_ui_token = storage_ui_token
+        # self.includes_va = includes_va
+        # self.va_ip = va_ip
+        # self.va_register = va_register
+        # self.includes_storage_ui = includes_storage_ui
+        # self.storage_ui_ip = storage_ui_ip
+        # self.storage_ui_register = storage_ui_register
+        # self.storage_ui_token = storage_ui_token
 
         # Public net
         self.public_ip = public_ip
@@ -74,31 +72,12 @@ class VzVmConfig:
         self.public_gw = VzVmBaseConfig.public_gw
         self.public_dns = VzVmBaseConfig.public_dns
 
-        # Private net only when needed. Default False
-        self.private_ip = private_ip
-        self.private_mask = "255.255.255.0"
-        if self.private_ip:
-            self.private_net = "pn_for_" + VzVmBaseConfig.cluster_name
-
         # VNC settings
         self.vnc_mode = "auto"
         self.vnc_passwd = "--vnc-nopasswd"
 
         # Description for IP
         self.ips = self.ips + self.public_ip
-
-        # Add IP to description if needed
-        if self.includes_va:
-            self.ips = self.ips + " " + self.va_ip
-        elif self.va_register:
-            self.ips = self.ips + " " + self.va_ip
-
-        if self.includes_storage_ui:
-            self.ips = self.ips + " " + self.storage_ui_ip
-        elif self.storage_ui_register:
-            self.ips = self.ips + " " + self.storage_ui_ip
-
-        self.description = "extip:[{}]".format(self.ips)
 
         # libvirt specific options
         self.domainxml = os.path.join(VzVmBaseConfig.vm_config_dir, self.name)
@@ -110,7 +89,18 @@ class VzVmConfig:
         self.vmlinuz = iso.get_vmlinuz()
         self.initrd = iso.get_initrd()
 
+                            # kickstart requires the following:
+                            #"ksdevice"
+                            #"ip"
+                            #"netmask"
+                            # so I TEMPORARY add this
+                            # to make sure that my 
+                            # example.cfg will be able to run
+
         self.commandline = ("inst.stage2=hd:LABEL={} "
+                            "ksdevice={}"
+                            "ip={}"
+                            "netmask={}"
                             "hostname={} "
                             "ks_device={} "
                             "public_ip={} "
@@ -118,14 +108,11 @@ class VzVmConfig:
                             "public_gw={} "
                             "public_dns={} "
                             "ks={} "
-                            "includes_va={} "
-                            "va_ip={} "
-                            "includes_storage_ui={} "
-                            "storage_ui_ip={} "
-                            "private_ip={} "
-                            "private_mask={}"
                             ).format(
                                     iso.name,
+                                    self.ks_device,
+                                    self.public_ip,
+                                    self.public_mask,
                                     self.hostname,
                                     self.ks_device,
                                     self.public_ip,
@@ -133,13 +120,71 @@ class VzVmConfig:
                                     self.public_gw,
                                     self.public_dns,
                                     self.ks,
-                                    self.includes_va,
-                                    self.va_ip,
-                                    self.includes_storage_ui,
-                                    self.storage_ui_ip,
-                                    self.private_ip,
-                                    self.private_mask
                                     )
+
+        # Private net only when needed. Default False
+        if private_ip:
+            self.private_ip = private_ip
+            self.private_mask = "255.255.255.0"
+            self.private_net = "pn_for_" + VzVmBaseConfig.cluster_name
+            self.commandline = self.commandline +   ("private_ip={} "
+                                                    "private_mask={} "
+                                                    "private_net "
+                                                    ).format(
+                                                            self.private_ip,
+                                                            self.private_mask,
+                                                            self.private_net
+                                                            )
+
+        # VA options if needed. Default False
+        # Add related options to commandline
+        if includes_va and va_ip:
+            self.includes_va = includes_va
+            self.va_ip = va_ip
+            self.va_register = True
+            self.ips = self.ips + " " + self.va_ip
+            self.commandline = self.commandline +   ("includes_va={} "
+                                                    "va_ip={} "
+                                                    ).format(
+                                                            self.includes_va,
+                                                            self.va_ip
+                                                            )
+        elif va_register and va_ip:
+            self.va_ip = va_ip
+            self.va_register = va_register
+            self.commandline = self.commandline +   ("va_register={} "
+                                                    "va_ip={} "
+                                                    ).format(
+                                                            self.va_register,
+                                                            self.va_ip
+                                                            )
+        # Storage UI options if needed. Default False.
+        # Add related options to commandline.
+        if includes_storage_ui and storage_ui_ip:
+            self.includes_storage_ui = includes_storage_ui
+            self.storage_ui_ip = storage_ui_ip
+            self.ips = self.ips + " " + self.storage_ui_ip
+            self.commandline = self.commandline +   ("includes_storage_ui={} "
+                                                    "storage_ui_ip={} "
+                                                    ).format(
+                                                            self.includes_storage_ui,
+                                                            self.storage_ui_ip
+                                                            )
+
+        elif storage_ui_register and storage_ui_ip and storage_ui_token:
+            self.storage_ui_register = storage_ui_register
+            self.storage_ui_ip = storage_ui_ip
+            self.storage_ui_token = storage_ui_token
+            self.commandline = self.commandline +   ("storage_ui_register={} "
+                                                    "storage_ui_ip={} "
+                                                    "storage_ui_token={} "
+                                                    ).format(
+                                                            self.includes_storage_ui,
+                                                            self.storage_ui_ip,
+                                                            self.storage_ui_token
+                                                            )
+
+        self.description = "extip:[{}]".format(self.ips)
 
     def create(self):
         # Create the VM
@@ -208,19 +253,38 @@ class VzVmConfig:
 
             for index, line in enumerate(tmp):
                 if "<qemu:commandline>" in line:
+                    # Need to rewrite for XML
+                    # This is not cool at all
                     tmp.insert(index + 1,
-                            "    <qemu:arg value='-kernel {}"
-                            " -initrd {} "
-                            " -append {}'/>\n".format(
-                                self.vmlinuz, 
-                                self.initrd, 
-                                self.commandline
-                                )
+                            "    <qemu:arg value='{}'/>\n".format(self.vmlinuz)
+                            )
+                    tmp.insert(index + 1,
+                            "    <qemu:arg value='-kernel'/>\n"
+                            )
+                    tmp.insert(index + 1,
+                            "    <qemu:arg value='{}'/>\n".format(self.initrd)
+                            )
+                    tmp.insert(index + 1,
+                            "    <qemu:arg value='-initrd'/>\n"
+                            )
+                    tmp.insert(index + 1,
+                            "    <qemu:arg value='{}'/>\n".format(self.commandline)
+                            )
+                    tmp.insert(index + 1,
+                            "    <qemu:arg value='-append'/>\n"
                             )
 
             with open(self.domainxml_temp, 'w') as file:
                 for line in tmp:
                     file.write(line)
+
+    def define(self, domainxml):
+        # This execute virsh define <domainxml>
+        subprocess.call(["virsh", "define", domainxml], stdout = False)
+
+    def start(self):
+        # This start the VM
+        subprocess.call(["prlctl", "start", self.name], stdout = False)
     
 
 
@@ -286,3 +350,18 @@ class iso:
                 self.mount_dir
                 ))
 # Some kind of "main" is required here. Will add later.
+#
+#
+#
+#
+# base = c.VzVmBaseConfig("test_cluster", "/mnt/sin/iso/vz-iso-7.0.9-534.iso")
+# iso = c.iso(base.vm_iso_path, base.mount_dir)
+# vm = c.VzVmConfig(base, iso, "kchestnov_script_test", "172.16.56.201", includes_va=True, va_ip="172.16.56.202", includes_storage_ui=True, storage_ui_ip="172.16.56.203")
+# vm.create()
+# vm.add_kernel_options()
+# vm.define(vm.domainxml_temp)
+# vm.start()
+# WAIT UNTIL FINISH
+# vm.define(vm.domainxml)
+# vm.start()
+
