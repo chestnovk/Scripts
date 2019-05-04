e403dceabf3e105ba33e76575659bd158795b1ac
Konstantin Chestnov <chestnov.konstantin@yandex.ru>
2019-05-04 12:57:22 +0300

    cluster_config.json was modified
    json_parser.py in progress

--- json_parser.py
+++ json_parser.py
@@ -1,12 +1,43 @@
 #!/usr/bin/python3.6
 
 import json
+import create_vm_tmp as script
+
+print('''We have successfully import create_vm_tmp
+        here is a startup parameters:
+        cluster_name: {}
+        iso_path: {}
+        base_config: {}
+        vm_iso: {}
+        config_path: {}
+        list_of_vm: {}'''.format(
+            script.cluster_name,
+            script.iso_path,
+            script.base_config,
+            script.vm_iso,
+            script.config_path,
+            script.list_of_vm)
+        )
+
+print('''Now we are going to create a VzVmBaseConfig:
+        To do so we will need to get the following parameters:
+        cluster_name
+        vm_iso_path''')
+
 
 with open("cluster_config.json") as f:
-    config = json.load(f)
-    for item in config.items():
-        if item[0] == 'cluster':
-                name = item[1]['Name']
-                print(name)
 
+    config = json.load(f)
+
+    cluster = config.get('cluster')
+    name = cluster.get('name')
+    iso = cluster.get('iso')
+
+    print('Found name: {}'.format(name))
+    print('Found iso: {}'.format(iso))
+
+    vms = config.get('vm')
+
+    for vm in vms.values():
+        print(vm['name'])
 
